7414bc48916602b900cf3b130842a9940a574dc2
qiuyuanshun <qiuyuanshun@yeah.net>
2016-02-25 23:09:49 +0800

    test

---
+++ first_day/test.cpp
@@ -0,0 +1,11 @@
+ ///---------------------------------------///
+ /// @file is the test.cpp
+ /// @arthor is the arthur(qiuyuanshun@yeah.net)
+ /// @date:2016-02-25 23:09:12
+ ///------------------------------------------///
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+}
