6c6677a87c6b49ee5b628e830cb48a8605c57887
qiuyuanshun <qiuyuanshun@yeah.net>
2016-02-29 22:45:15 +0800

    1 change

--- MutexLock.cc
+++ MutexLock.cc
@@ -18,10 +18,10 @@ class MutexLock
             pthread_mutex_destroy(&_mutex);
         }
 
-        /*pthread_mutex_t * getLock()
+        pthread_mutex_t * getLock()
         {
-            return &mutex_;
-        }*/
+            return &_mutex;
+        }
         void Lock()
         {
             pthread_mutex_lock(&_mutex);
@@ -33,7 +33,7 @@ class MutexLock
         }
     private:
         pthread_mutex_t _mutex;
-        MutexLock(const MutexLock &);
+        MutexLock(const MutexLock &);//禁止复制
         MutexLock & operator = (const MutexLock &);
 }; 
 class Condittion 
@@ -50,7 +50,7 @@ class Condittion
         }
         void Condittion_wait()
         {
-            pthread_cond_wait(&cond,mutex);
+            pthread_cond_wait(&cond,_mutex.getLock());
         }
         void Condittion_signal()
         {
@@ -63,4 +63,34 @@ class Condittion
     private:
         pthread_cond_t cond;
         MutexLock & _mutex; 
-}; 
+};
+
+int sum = 0;
+void * p_func(void *p)
+{
+    int i;
+    MutexLock * mutex = (MutexLock *) p;
+   for(i = 0;i<1000000;i++)
+   {
+       (*mutex).Lock();
+       ++sum;
+       (*mutex).unlock();
+   }
+   std::cout << sum << std::endl;
+}
+int main()
+{
+    MutexLock mutex;
+    pthread_t pthid;
+    pthread_create(&pthid,NULL,p_func,(void *)&mutex);
+    int i;
+    for(i=0;i<1000000;i++)
+    {
+        mutex.Lock();
+        sum++;
+        mutex.unlock();
+    }
+    std::cout << sum << std::endl;
+    pthread_join(pthid,NULL);
+
+}
